fe3d1ce60211e3d8c396389f7cee3e9dc67bc49a
Gennadiy99 <login841@gmail.com>
2025-11-08 12:57:08 +0100

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logoMate = document.querySelector('.logo');
+const divResolve = document.createElement('div');
+
+const prom1 = new Promise((resolve, reject) => {
+  logoMate.addEventListener('click', () => {
+    resolve('Promise was solved!');
+  });
+
+  setTimeout(reject, 5000, 'Promise was denied!');
+});
+
+prom1.then((respons) => {
+  if (respons) {
+    // const divResolve = document.createElement('div');
+
+    divResolve.innerHTML = respons;
+    divResolve.classList.add('message');
+
+    document.body.append(divResolve);
+  }
+});
+
+prom1.catch((error) => {
+  // const divResolve = document.createElement('div');
+
+  divResolve.innerHTML = error;
+  divResolve.classList.add('message', 'error-message');
+
+  document.body.append(divResolve);
+});
